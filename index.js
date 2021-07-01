e95edb530535ad5bcad28f907b774f5ce2a7c35f
Erik Gross <erik@developintelligence.com>
2021-07-01 13:59:09 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let lodash = require('lodash');
+
+let name = "erik";
+
+name = lodash.upperFirst(name);
+
+console.log(name);
+
+// use these functions from lodash:
+// lowerCase();
+// upperCase();
+// startCase();
+
